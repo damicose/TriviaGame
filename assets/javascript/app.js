1f9e8ab0c27000e2fc5af574e11c140f6ef5da71
Sonny D <damicose14@gmail.com>
2019-08-08 14:20:09 -0500

    pseudo code of requirements

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Only allow one answer (activated button??) per question, can toggle between choices
+
+// Set total time for quiz (operating in background)--click button to begin timer/show questions?
+// Create different alerts depending on whether time elapsed (show time remaining stat?)
+
+// Track which answer options for each question are correct/incorrect
+// Programatically evaluate right/wrong and spit out those values
+
+// Reset game upon clicking an 'ok' button on right/wrong screen (no refresh)
