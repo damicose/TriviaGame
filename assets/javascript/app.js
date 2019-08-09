69a28df0d9e5b7b33408107706033ff9fd9de73b
Sonny D <damicose14@gmail.com>
2019-08-08 19:54:12 -0500

    checkpoint-ing, written up untested java and pared down number of questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,59 @@
 let right = 0
 let wrong = 0
 
-function checkAns() {
-if
+function chose() {
+    classList.add("chosen")
 }
+
+function results(){
+    document.getElementById("quest").innerHTML = "<h1>You got " + right + " questions right and " + wrong + " questions wrong!</h1>";
+document.createElement("button")
+}
+
+document.getElementById("1customRadioInline2").addEventListener("change", chose())
+document.getElementById("2customRadioInline3").addEventListener("change", chose())
+document.getElementById("3customRadioInline1").addEventListener("change", chose())
+document.getElementById("4customRadioInline4").addEventListener("change", chose())
+document.getElementById("5customRadioInline2").addEventListener("change", chose())
+
+
+
+
+
+
+function checkAns() {
+    if (document.getElementById("1customRadioInline2").contains("chosen")) {
+        right++
+    }
+    else {
+        wrong++
+    }
+    if (document.getElementById("2customRadioInline3").contains("chosen")) {
+        right++
+    }
+    else {
+        wrong++
+    }
+    if (document.getElementById("3customRadioInline1").contains("chosen")) {
+        right++
+    }
+    else {
+        wrong++
+    }
+    if (document.getElementById("4customRadioInline4").contains("chosen")) {
+        right++
+    }
+    else {
+        wrong++
+    }
+    if (document.getElementById("5customRadioInline2").contains("chosen")) {
+        right++
+    }
+    else {
+        wrong++
+    }
+    results()
+}
+
+
 // Reset game upon clicking an 'ok' button on right/wrong screen (no refresh)
